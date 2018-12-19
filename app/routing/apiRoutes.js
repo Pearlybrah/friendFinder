b13f418d8becba767e4af941b2f0a910053de0ba
Zachary Ferreira <Pearlybrah@github.com>
2018-12-19 15:11:30 -0800

    finished, needs some styling but is functional

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,9 +8,28 @@ module.exports = function(app) {
         res.json(friendData);
     });
 
-    app.post("/api/friends", function(req, res) {
-        friendData.push(req.body);
-        res.json(true);
+    app.post("/api/friends", function (req, res) {
+
+        var userData = req.body;
+        var closestFriendIndex = 0;
+        var smallestDifference = 40;
+    
+        for (i = 0; i < friendData.length; i++) {
+          var actualDifference = 0;
+          for (x = 0; x < 10; x++) {
+            var difference = Math.abs(userData.scores[x] - friendData[i].scores[x]);
+            actualDifference += difference;
+          }
+    
+          if (actualDifference < smallestDifference) {
+            closestFriendIndex = i;
+            smallestDifference = actualDifference;
+          }
+        };
+    
+        friendData.push(userData);
+        res.json(friendData[closestFriendIndex]);
     });
+    
     
 };
